9b931b52c874aed441bdca1c1824999af156ad69
Olga Zarosylova <zoa1906@gmail.com>
2022-12-16 20:46:29 +0200

    //new

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
+    public static void main(String[] args) {
+        CoinFlip coinFlip = new CoinFlip();
+        coinFlip.throwCoin(400);
+    }
     public void throwCoin(int from1UpTo1000) {
-
+        if(from1UpTo1000 < 500) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
+    public static void main(String[] args) {
+        GreatestNumberPrinter greatestNumberPrinter = new GreatestNumberPrinter();
+        greatestNumberPrinter.printGreatest(5,7);
+    }
     public void printGreatest(int first, int second) {
+        int max = first > second ? first : second;
     }
 }
